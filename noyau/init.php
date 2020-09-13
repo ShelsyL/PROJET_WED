c8e88891a6a4dee4001deb7c42e1df651bf9bc06
Chelsea <chelsealouis.graphiste@gmail.com>
2020-09-13 13:29:04 +0200

    Structure MVC vierge
    Mise en place d'une structure MVC vierge

---
+++ noyau/init.php
@@ -0,0 +1,11 @@
+<?php
+/*
+	./noyau/init.php
+    FICHIER D'INITIALISATION
+*/
+
+  // 1. PARAMETRES DE CONNEXIONS - On charge les paramètres
+	require_once '../app/config/parametres.php';
+
+  // 2. CONNEXION - On charge la connexion
+	require_once '../noyau/connexion.php';
